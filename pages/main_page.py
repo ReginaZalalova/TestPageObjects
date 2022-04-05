eab31f0ff2b8ea789903ebb4b432a16df5c442fa
ReginaZalalova <reginazalalova19@gmail.com>
2022-04-05 18:00:20 +0300

    added to main page first locator

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,10 @@
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
 
 class MainPage(BasePage):
      def should_be_login_link(self):
-        assert self.is_element_present(By.XPATH,'//a[@id="login_link"]'), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.login_link), "Login link is not presented"
      def go_to_login_page(self):
-        login_link = self.browser.find_element(By.XPATH,'//a[@id="login_link"]')
+        login_link = self.browser.find_element(*MainPageLocators.login_link)
         login_link.click()
