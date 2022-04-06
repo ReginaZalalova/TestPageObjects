9d0f27ae8285efb2a8fa3c8761cf337271ccd741
ReginaZalalova <reginazalalova19@gmail.com>
2022-04-06 15:44:13 +0300

    Login page added

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from pages.base_page import BasePage
+from pages.locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert self.is_element_present(*LoginPageLocators.login_url)
+        assert "login" in self.url, "Login link is not include word LOGIN"
+
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.login_form), "Login form is not presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.register_form), "Register form is not presented"
